d17e6f11132d86412092a397ae9609ab5f63051a
moncefplastin07 <moncefplastin07@gmail.com>
2022-10-13 17:16:16 +0100

    make global change in db

--- search.ts
+++ search.ts
@@ -1,5 +1,4 @@
-export const searchInDB = async (query: any = "", db = "" ) => {
-  
+export const searchInDB = async (query: any = "", db = "") => {
   const dbPath = {
     li: "./db/libraries.json",
     so: "./db/sociology.json",
@@ -7,21 +6,38 @@ export const searchInDB = async (query: any = "", db = "" ) => {
     co: "./db/communication.json",
     pc: "./db/psychology.json",
     fl: "./db/foreign_languages.json",
-    th: "./db/thakafa.json"
-  }[db]
+    th: "./db/thakafa.json",
+  }[db];
   if (dbPath == null) {
-    return []
+    return [];
   }
   const db_ = JSON.parse(await Deno.readTextFile(dbPath));
   const keywords = query.split(" ")
-    .filter((keyword:string)=>{
-        if (keyword.length > 2) {
-            return keyword
-        }
-    })
-  const keywordsRegEx = new RegExp(keywords.join('|'), "g")
-  const searchResult = db_.filter((book: any) =>
-  new Set(book.title.match(keywordsRegEx)).size >= keywords.length || new Set(book.author.match(keywordsRegEx)).size >= keywords.length || book.ID?.startsWith(query) 
-    );
-    return searchResult
+    .filter((keyword: string) => {
+      if (keyword.length > 2) {
+        return keyword;
+      }
+    });
+  const keywordsRegEx = new RegExp(keywords.join("|"), "g");
+
+  const searchResult = db_.filter((book: any) => {
+    book.matchRatio =
+      (new Set(book.title?.match(keywordsRegEx)).size / keywords.length * 100) +
+      (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length * 100) +
+      (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length * 100) +
+      (new Set(book.keywords?.match(keywordsRegEx)).size / keywords.length *
+          100) / 4;
+    return (new Set(book.title?.match(keywordsRegEx)).size / keywords.length *
+          100) >= 70 ||
+          (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length *
+          100) >= 70 ||
+          (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length *
+          100) >= 70 ||
+          (new Set(book.keywords?.match(keywordsRegEx)).size / keywords.length *
+          100) >= 70 ||
+      (new Set(book.author?.match(keywordsRegEx)).size / keywords.length *
+          100) >= 70 ||
+      book.ID?.startsWith(query);
+  });
+  return searchResult;
 };
