3c1b657a8c2b421c39e77443208d896458a961cc
moncefplastin07 <moncefplastin07@gmail.com>
2022-10-14 09:32:57 +0100

    Sort search results by most matches + deno fmt

--- search.ts
+++ search.ts
@@ -23,21 +23,25 @@ export const searchInDB = async (query: any = "", db = "") => {
   const searchResult = db_.filter((book: any) => {
     book.matchRatio =
       (new Set(book.title?.match(keywordsRegEx)).size / keywords.length * 100) +
-      (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length * 100) +
-      (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length * 100) +
+      (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length *
+        100) +
+      (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length *
+        100) +
       (new Set(book.keywords?.match(keywordsRegEx)).size / keywords.length *
           100) / 4;
     return (new Set(book.title?.match(keywordsRegEx)).size / keywords.length *
           100) >= 70 ||
-          (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length *
+      (new Set(book.subtitle?.match(keywordsRegEx)).size / keywords.length *
           100) >= 70 ||
-          (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length *
+      (new Set(book.notecontent?.match(keywordsRegEx)).size / keywords.length *
           100) >= 70 ||
-          (new Set(book.keywords?.match(keywordsRegEx)).size / keywords.length *
+      (new Set(book.keywords?.match(keywordsRegEx)).size / keywords.length *
           100) >= 70 ||
       (new Set(book.author?.match(keywordsRegEx)).size / keywords.length *
           100) >= 70 ||
       book.ID?.startsWith(query);
-  });
-  return {keywords, searchResult};
+  }).sort((currentItem, nextItem) => {
+    return currentItem.matchRatio - nextItem.matchRatio;
+  }).reverse();
+  return { keywords, searchResult };
 };
