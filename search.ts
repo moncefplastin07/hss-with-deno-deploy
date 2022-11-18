33f122815a91b0f0b8c5814bff71f9eb131d519a
moncefplastin07 <moncefplastin07@gmail.com>
2022-11-18 08:38:35 +0100

    search algorithm improvement

--- search.ts
+++ search.ts
@@ -8,6 +8,35 @@ interface Book {
   keywords: string;
   matchRatio?: number;
 }
+interface BookMatchs {
+  title: number;
+  subtitle: number;
+  notecontent: number;
+  keywords: number;
+  author: number;
+}
+const getMatchinCount = (haystack: string, keywords: any) => {
+  const keywordsRegEx = new RegExp(
+    keywords.join("|").replace(/(أ|آ|إ)/, "ا"),
+    "g",
+  );
+  return new Set(
+    `${haystack}`.replace(/(أ|آ|إ)/, "ا").toLowerCase().match(keywordsRegEx),
+  ).size;
+};
+const getBookMatchinRatio = (bookMatchs: BookMatchs, keywordsCount: number) => {
+  return (((((bookMatchs.title /
+    keywordsCount * 100) * 6) +
+    ((bookMatchs.subtitle /
+      keywordsCount *
+      100) * 3) +
+    ((bookMatchs.notecontent /
+      keywordsCount *
+      100) * 2) +
+    ((bookMatchs.keywords /
+      keywordsCount *
+      100) * 3)) / 14) / 5);
+};
 export const searchInDB = async (query: any = "", db = "") => {
   const dbPath = {
     li: "./db/libraries.json",
@@ -18,49 +47,29 @@ export const searchInDB = async (query: any = "", db = "") => {
     fl: "./db/foreign_languages.json",
     ca: "./db/thakafa.json",
     th: "./db/theses.json",
-    ma: "./db/master.json"
+    ma: "./db/master.json",
   }[db];
   if (dbPath == null) {
     return [];
   }
   const db_ = JSON.parse(await Deno.readTextFile(dbPath));
-  const keywords = query.toLowerCase().split(" ")
-    .filter((keyword: string) => {
-      if (keyword.length > 2) {
-        return keyword;
-      }
-    });
-  const keywordsRegEx = new RegExp(keywords.join("|"), "g");
-
+  const keywords = query.match(/(\S){3,}/g);
   const searchResult = db_.filter((book: any) => {
-    const titleKeywords = book.title?.toLowerCase().split(" ")
-      .filter((keyword: string) => {
-        if (keyword.length > 2) {
-          return keyword;
-        }
-      });
-    const titleMatchRatio =
-      new Set(book.title?.toLowerCase().match(keywordsRegEx)).size /
+    const titleKeywords = book.title?.match(/(\S){3,}/g);
+    const { title, subtitle, author, notecontent, keywords: bookKeywords } =
+      book;
+    const titleMatchRatio = getMatchinCount(book.title, keywords) /
       titleKeywords.length * 100;
     const bookMatchs = {
-      title: new Set(book.title?.toLowerCase().match(keywordsRegEx)).size,
-      subtitle: new Set(book.subtitle?.toLowerCase().match(keywordsRegEx)).size,
-      notecontent:
-        new Set(book.notecontent?.toLowerCase().match(keywordsRegEx)).size,
-      keywords: new Set(book.keywords?.toLowerCase().match(keywordsRegEx)).size,
-      author: new Set(book.author?.toLowerCase().match(keywordsRegEx)).size,
+      title: getMatchinCount(title, keywords),
+      subtitle: getMatchinCount(subtitle, keywords),
+      notecontent: getMatchinCount(notecontent, keywords),
+      keywords: getMatchinCount(bookKeywords, keywords),
+      author: getMatchinCount(author, keywords),
     };
-    book.matchRatio = ((((((bookMatchs.title /
-      keywords.length * 100) * 6) +
-      ((bookMatchs.subtitle /
-        keywords.length *
-        100) * 3) +
-      ((bookMatchs.notecontent /
-        keywords.length *
-        100) * 2) +
-      ((bookMatchs.keywords /
-        keywords.length *
-        100) * 3)) / 14) / 5) + titleMatchRatio) / 2;
+
+    book.matchRatio =
+      (getBookMatchinRatio(bookMatchs, keywords.length) + titleMatchRatio) / 2;
     return (bookMatchs.title /
           keywords.length *
           100) >= 70 ||
